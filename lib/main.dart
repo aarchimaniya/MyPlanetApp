48882bb8452cd7cee5ae2987e4bc7b8dc9240c02
aarchimaniya <aarchimaniya7@gmail.com>
2024-09-16 15:47:13 +0530

    inital commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:solar_systems/view/screens/Splash_Sceen/splash_screen.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
+    ),
+  );
+}
